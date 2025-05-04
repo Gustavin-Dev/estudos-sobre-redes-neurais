85fe5ea9bd5b2faf22a31e9387f3e9752a2dda0f
gustavo <henriquesgustavo138@gmail.com>
2025-05-04 00:00:50 -0300

    usando a biblioteca numPy para otimizacao da funcao soma

--- perceptron1.py
+++ perceptron1.py
@@ -1,10 +1,11 @@
-entradas = [1,7,5]
-pesos = [0.8,0.1,0]
+import numpy as np
+
+
+entradas = np.array([1,7,5])
+pesos = np.array([0.8,0.1,0])
 
 def soma(entradas,pesos):
-    soma = 0
-    for i in range(3):
-        soma += entradas[i] * pesos[i]
+    soma = entradas.dot(pesos)
     return soma
 
 resultadoFuncaoSoma = soma(entradas,pesos) 
