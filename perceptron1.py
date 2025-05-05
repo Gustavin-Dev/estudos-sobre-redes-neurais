95a7406a51c8cee1b64c73b81cc639835c9eb7e1
gustavo <henriquesgustavo138@gmail.com>
2025-05-04 22:53:50 -0300

    treinando e testando o modelo com porta logica or

--- perceptron1.py
+++ perceptron1.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 entradas = np.array([[0,0],[0,1],[1,0],[1,1]])
-saidasEsperadas = np.array([0,0,0,1])
+saidasEsperadas = np.array([0,1,1,1])
 pesos = np.array([0.0,0.0])
 taxaAprendizagem = 0.1
 
